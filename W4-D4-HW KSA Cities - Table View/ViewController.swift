ad43206519b7a0f5197a87406072865cd0c48bd3
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2021-11-25 16:09:14 +0300

    Initial Commit

---
+++ W4-D4-HW KSA Cities - Table View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W4-D4-HW KSA Cities - Table View
+//
+//  Created by يعرُب on 20/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
